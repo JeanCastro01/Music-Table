f5a8eacc25b0e6b2ec3a1d30a42032b498ea4997
JeanCastro01 <jeancastro.contato@gmail.com>
2021-04-15 19:56:51 +0000

    music router

--- routes.js
+++ routes.js
@@ -1,14 +1,8 @@
 const express = require('express'),
 router = express.Router();
 
-var
-// itemCtrl = require('./item-controller'),
-userCtrl = require('./user-controller');
 
-// router.get('/hello', itemCtrl.getWorld);
-// router.get('/hello/:foo/:bar', itemCtrl.getWorldParams);
-// router.post('/hello', itemCtrl.postWorld);
-
+var userCtrl = require('./user-controller');
 router.post('/users', userCtrl.createUser);
 router.get('/users', userCtrl.getUsers);
 router.get('/users/:id', userCtrl.getUser);
@@ -19,17 +13,20 @@ module.exports.UPLOAD_PATH = "uploads";
 
 var multer = require("multer");
 var upload = multer({ dest: module.exports.UPLOAD_PATH});
+
+
 var imageCtrl = require('./image-controller');
-
-router.post('/images', upload.single('image'), imageCtrl.uploadImage);
-router.get('/images', imageCtrl.getImages);
-
-
-
-
 router.post('/images', upload.single('image'), imageCtrl.uploadImage);
 router.get('/images', imageCtrl.getImages);
 router.get('/images/:id', imageCtrl.getImage);
 router.delete('/images/:id', imageCtrl.deleteImage);
+
+
+var musicCtrl = require('./music-controller');
+router.post('/music', musicCtrl.createMusic);
+router.get('/music', musicCtrl.getMusic);
+router.get('/music/:id', musicCtrl.getMusic);
+router.put('/music/:id', musicCtrl.updateMusic);
+router.delete('/music/:id', musicCtrl.deleteMusic);
 
 module.exports = router;
