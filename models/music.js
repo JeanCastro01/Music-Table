91578f3eb89d117dfd2fcf8c27d82a46e2986a9d
JeanCastro01 <jeancastro.contato@gmail.com>
2021-04-20 18:33:19 +0000

    deleted some files

--- models/music.js
+++ models/music.js
@@ -1,43 +1,14 @@
-// var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
-// var musicSchema = new mongoose.Schema({ 
-//     artistname: String,
-//     albumname: String,
-//     yearofrelease: String,
-//     recordlabelname: String,
-
-// },
-// { timestamps: true }
-// );
-
-// module.exports = mongoose.model('music', musicSchema);
-
-const mongoose = require('mongoose');
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-
-app.use(bodyParser.urlencoded({extended: true}));
-
-
-const musicSchema = { 
+var musicSchema = new mongoose.Schema({ 
     artistname: String,
     albumname: String,
     yearofrelease: String,
     recordlabelname: String,
 
-}
+},
+{ timestamps: true }
+);
 
-const Note = mongoose.model('musics', musicSchema);
+module.exports = mongoose.model('music', musicSchema);
 
-app.post("/" , function(req, res) { 
-    let newmusic = new Note({
-    artistname: req.body.artistname,
-    albumname: req.body.albumname,
-    yearofrelease: req.body.yearofrelease,
-    recordlabelname: req.body.recordlabelname
-    });
-    
-    newmusic.save();
-     res.redirect("/")
-})
